fdb4707bbe9078afea6dff98f956475b7dc645d0
Maxim Sokolov <107122685+VozunMaxim@users.noreply.github.com>
2022-07-30 23:49:29 +0300

    Update StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,7 +16,7 @@ public class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
     @Test
-    void findMax22() {
+    void findMaxTotal() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12 + 5 + 8 + 4 + 5 + 3 + 8 + 6 + 11 + 11 + 12};
